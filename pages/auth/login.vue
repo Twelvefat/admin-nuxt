86e50cc05f0ef9f7a4f003c72dac0f690803d51b
Rizal Fatahillah <rizalfatahillah77@gmail.com>
2020-02-16 19:38:11 +0700

    login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1> Login Page </h1>
+  </div>
+</template>
+<script>
+import {mapGetters, mapMutations} from 'vuex'
+export default {
+  layout: 'auth',
+  fetch(){
+  },
+  computed: {
+    ...mapGetters({
+      user:'user/user',
+      token:'user/token',
+    })
+  },
+}
+</script>
