a6dfdad42afcf83e9f8029f6c06caedcc0f03c2d
Rizal Fatahillah <rizalfatahillah77@gmail.com>
2020-08-22 08:15:01 +0700

    add loading and custom footer

--- components/partials/role/ModalDetail.vue
+++ components/partials/role/ModalDetail.vue
@@ -1,16 +1,29 @@
 <template>
-  <div v-if="data">
+  <div>
     <a-modal v-model="visible" title="Detail Role">
-      <h3>{{data.name | capitalize}}</h3>
-      <a-form-item label="Permissions">
-          <a-row>
-            <a-col :span="11" v-for="(permission, index) in data.rolePermissions" :key="index">
-              <a-checkbox :value="permission.id" :checked="true">
-                {{permission.name}}
-              </a-checkbox>
-            </a-col>
-          </a-row>
-      </a-form-item>
+      <a-spin :spinning="!data.hasOwnProperty('rolePermissions')">
+
+        <!-- Custom icon spin -->
+        <a-icon slot="indicator" type="loading" style="font-size: 24px" spin />
+
+        <h3>{{data.name | capitalize}}</h3>
+        <a-form-item label="Permissions">
+            <a-row>
+              <a-col :span="11" v-for="(permission, index) in data.rolePermissions" :key="index">
+                <a-checkbox :value="permission.id" :checked="true">
+                  {{permission.name | capitalize}}
+                </a-checkbox>
+              </a-col>
+            </a-row>
+        </a-form-item>
+      </a-spin>
+
+      <!-- Footer -->
+      <template slot="footer">
+        <a-button key="back" @click="handleCancel">
+          Return
+        </a-button>
+      </template>
     </a-modal>
   </div>
 </template>
@@ -20,10 +33,15 @@ export default {
   props: ['data','visible'],
   filters: {
     capitalize: function (value) {
-    if (!value) return ''
-    value = value.toString()
-    return value.charAt(0).toUpperCase() + value.slice(1)
-  }
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  methods: {
+    handleCancel(){
+      this.visible = false
+    }
   }
 }
 </script>
